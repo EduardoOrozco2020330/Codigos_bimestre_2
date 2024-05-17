84913d5002c11303c13b8d7c317c4f6c16067357
EduardoOrozco2020330 <143019071+EduardoOrozco2020330@users.noreply.github.com>
2024-05-17 10:22:04 -0600

    Add files via upload

--- P6__p1/P6__p1.ino
+++ P6__p1/P6__p1.ino
@@ -17,17 +17,11 @@ LiquidCrystal LCD_1(RS, Enable, D4, D5, D6, D7); //Constructor que me permite us
 void setup()
 {
   LCD_1.begin(16,2); //Objeto de una LCD de 16 columnas x 2 filas
-  LCD_1.setCursor(4,0);
+  LCD_1.setCursor(0,0);
   LCD_1.print("Eduardo");
-  LCD_1.setCursor(4,2);
+  LCD_1.setCursor(0,2);
   LCD_1.print("2020330");
 }
 void loop()
 {
-  LCD_1.setCursor(0,1);  //Primera columna y segunda fila  
-  LCD_1.write(1);
-  delay(250);
-  LCD_1.setCursor(0,2);
-  LCD_1.write(2);
-  delay(250);
 }
